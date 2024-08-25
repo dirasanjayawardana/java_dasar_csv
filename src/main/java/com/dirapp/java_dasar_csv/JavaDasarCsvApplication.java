350548dcd37cd7f33396896ba010445346d6affd
dirasanjayawardana <dirawardana.dw@gmail.com>
2024-08-25 13:59:16 +0700

    backup

---
+++ src/main/java/com/dirapp/java_dasar_csv/JavaDasarCsvApplication.java
@@ -0,0 +1,13 @@
+package com.dirapp.java_dasar_csv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDasarCsvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDasarCsvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dirapp/java_dasar_csv/JavaDasarCsvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dirapp.java_dasar_csv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDasarCsvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
